41fdc53cac7b15a6aa2ef232c0a3fdbf36155569
ChrisAchinga <achinga.chris@gmail.com>
2020-09-26 22:41:51 +0300

    operating system module

---
+++ docs/os.js
@@ -0,0 +1,28 @@
+const os = require("os");
+
+// Check platform
+console.log(os.platform());
+
+// CPU architecture
+console.log(os.arch())
+
+// cpu core info
+console.log(os.cpus())
+
+// free memory
+console.log(os.freemem())
+
+// total mem
+console.log(os.totalmem())
+
+// network
+console.log(os.networkInterfaces())
+
+// os version
+console.log(os.version())
+
+// Home dir
+console.log(os.homedir());
+
+// Uptime
+console.log(os.uptime());
